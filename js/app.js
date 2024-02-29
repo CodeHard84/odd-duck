8584897c100d9c96bf743fe8661898da5bf78c83
Jace <sockgnome@att.net>
2024-02-28 20:50:12 -0600

    Loaded the products into an array.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,71 @@
 'use strict';
 
-// Global Variables
+//----- Global Variables -----//
 
-// Constructors
+let products = [];
 
-// Methods
+//----- Constructors ----- //
 
-// Functions
+function Product(name, src) {
+  this.name = name;
+  this.src = src;
+  this.views = 0;
+}
+
+//----- Methods -----//
+
+// Going to add a shuffle method to Array. This could be done by just
+// creating a shuffle function, but I like this better.
+
+Array.prototype.shuffle = function () {
+  // Fisher-Yates shuffle.
+  for (let i = this.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [this[i], this[j]] = [this[j], this[i]];
+  }
+  return this;
+};
+
+//----- Functions -----//
+
+//----- Load the array -----//
+
+// Make an object holding all of the products.
+// Going to make a function to avoid an RSI.
+
+// ChatGPT helped with this... Using prompt:
+// make a JS object that uses the jpg's filename as name and
+// the filename as src with src being located ./img/ using this list
+// and Now make a function to push that to the products array
+
+// banana.jpg    boots.jpg      bubblegum.jpg  cthulhu.jpg
+// dragon.jpg  pet-sweep.jpg  shark.jpg  tauntaun.jpg  water-can.jpg
+// bag.jpg    bathroom.jpg  breakfast.jpg  chair.jpg
+// dog-duck.jpg  pen.jpg     scissors.jpg   sweep.png
+// unicorn.jpg   wine-glass.jpg
+
+const fileNames = [
+  'banana.jpg', 'boots.jpg', 'bubblegum.jpg', 'cthulhu.jpg', 'dragon.jpg',
+  'pet-sweep.jpg', 'shark.jpg', 'tauntaun.jpg', 'water-can.jpg', 'bag.jpg',
+  'bathroom.jpg', 'breakfast.jpg', 'chair.jpg', 'dog-duck.jpg', 'pen.jpg',
+  'scissors.jpg', 'sweep.png', 'unicorn.jpg', 'wine-glass.jpg'
+];
+
+const imgObject = {};
+
+fileNames.forEach(fileName => {
+  const name = fileName.split('.')[0];
+  const src = `./img/${fileName}`;
+  imgObject[name] = src;
+});
+
+function populateProductsArray(fileNames, products) {
+  fileNames.forEach(fileName => {
+    const name = fileName.split('.')[0];
+    const src = `./img/${fileName}`;
+    products.push({ name, src });
+  });
+}
+
+populateProductsArray(fileNames, products);
+console.log(products);
