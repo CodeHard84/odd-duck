80e19c7b78ebbbcdd0bd07f96c7a580b8642b59e
Jace <sockgnome@att.net>
2024-02-28 21:17:50 -0600

    End of night commit.

--- js/app.js
+++ js/app.js
@@ -26,7 +26,23 @@ Array.prototype.shuffle = function () {
   return this;
 };
 
+// Add a method to Product to generate a number of random products from
+// products array.
+
+// Product.prototype.genProducts = function(number) {
+// };
+
+// I am going to make the above a function for now because I am not sure if
+// putting the method in the constructor is appropriate here...
+
 //----- Functions -----//
+
+function genProducts(number) {
+  // Grab a number of random products from the productsTmp array.
+  while(number > 0) {
+    number--;
+  }
+}
 
 //----- Load the array -----//
 
@@ -59,6 +75,9 @@ fileNames.forEach(fileName => {
   imgObject[name] = src;
 });
 
+// TODO: I need to make this create an instance of a product using the
+// constructor.
+
 function populateProductsArray(fileNames, products) {
   fileNames.forEach(fileName => {
     const name = fileName.split('.')[0];
@@ -67,5 +86,18 @@ function populateProductsArray(fileNames, products) {
   });
 }
 
+// End GPT.
+
+//----- Kickoff -----//
+
+// Populate the array
 populateProductsArray(fileNames, products);
+const productsTmp = products;
+
+// Shuffle the array using our new method shuffle.
+products = products.shuffle();
 console.log(products);
+
+// Generate some products.
+genProducts(3);
+
